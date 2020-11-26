dce0f1ee19485cf681d8a0185e09c68eda6ed71d
ruohaige <geruohai97@outlook.com>
2020-11-26 15:52:03 -0500

    add multiple objects && add noise && add split objects

--- util.py
+++ util.py
@@ -8,33 +8,32 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
+
 ModelNet_category = ["airplane","bathtub","bed","bench","bookshelf","bottle","bowl","car","chair","cone","cup","curtain","desk","door","dresser","flower_pot","glass_box","guitar","keyboard","lamp","laptop","mantel","monitor","night_stand","person","piano","plant","radio","range_hood","sink","sofa","stairs","stool","table","tent","toilet","tv_stand","vase","wardrobe","xbox"]
 
 ''' 
 generatePcd : randomly chose pcd to generate test cases
 Input : 
     test_group -> the number of tests need to be created
+    object_num -> the number of pcd need to be created
 Output: 
-    pcd1_arr -> pcd1 randomly chosen to generate testcase (1*test_group array)
-    pcd2_arr -> pcd2 randomly chosen to generate testcase (1*test_group array)
+    pcd_arrs -> randomly chosen pcd to generate testcase (object_num*test_group array)
 '''
-def generatePcd(test_group):
-    category_arr_1 = np.random.randint(len(ModelNet_category), size=test_group)
-    category_arr_2 = np.random.randint(len(ModelNet_category), size=test_group)
+def generatePcd(test_group, object_num):
+    category_arrs = []
+    for _ in range(0, object_num):
+        category_arrs.append(np.random.randint(len(ModelNet_category), size=test_group))
 
-    pcd1_arr = []
-    pcd2_arr = []
-    for index in range(0,test_group):
-        filename_1 = random.choice(os.listdir("pcd_data/"+str(ModelNet_category[category_arr_1[index]])+"/"))
-        filename_1 = "pcd_data/"+str(ModelNet_category[category_arr_1[index]])+"/" + filename_1
-        pcd1 = o3d.io.read_point_cloud(filename_1)
-        pcd1_arr.append(pcd1)
-
-        filename_2 = random.choice(os.listdir("pcd_data/"+str(ModelNet_category[category_arr_2[index]])+"/"))
-        filename_2 = "pcd_data/"+str(ModelNet_category[category_arr_2[index]])+"/" + filename_2
-        pcd2 = o3d.io.read_point_cloud(filename_2)
-        pcd2_arr.append(pcd2)
-    return pcd1_arr, pcd2_arr
+    pcd_arrs = []
+    for category_arr in category_arrs:
+        pcd_arr = []
+        for index in range(0,test_group):
+            filename = random.choice(os.listdir("pcd_data/"+str(ModelNet_category[category_arr[index]])+"/"))
+            filename = "pcd_data/"+str(ModelNet_category[category_arr[index]])+"/" + filename
+            pcd1 = o3d.io.read_point_cloud(filename)
+            pcd_arr.append(pcd1)
+        pcd_arrs.append(pcd_arr)
+    return pcd_arrs
 
 ''' 
 transformPcd : transform pcd with the given transform element including R and T
@@ -58,127 +57,177 @@ def transformPcd(pcd, t_and_r):
 generateTestCase : generate test case of original pcd points' locations 
                     and transformed pcd points' locations
 Input : 
+    object_num -> the number of pcd need to be created
     test_group -> the number of tests need to be created
     rot_file_name -> the transform's elements file need to be chosen
                     (3 different rotation's limits 45, 90,180)
-    pcd1_arr -> pcd1 randomly chosen to generate testcase (1*test_group array)
-    pcd2_arr -> pcd2 randomly chosen to generate testcase (1*test_group array)
+    pcd_arrs -> randomly chosen pcd to generate testcase (object_num*test_group array)
 Output: 
     test_cases written to the "./test_cases/" folder
 '''
-def generateTestCase(test_group, rot_file_name, pcd1_arr, pcd2_arr):
+def generateTestCase(object_num, test_group, rot_file_name, pcd_arrs):
     os.makedirs("./test_cases/",exist_ok=True)
     rot_arr = np.loadtxt(open(rot_file_name, "rb"), delimiter=",")
-    transform_index_pcd_1 = np.random.randint(rot_arr.shape[0], size=test_group)
-    transform_index_pcd_2 = np.random.randint(rot_arr.shape[0], size=test_group)
+
+    original_pcd_transform_index_arr = []
+    transformed_pcd_transform_index_arr = []
+    for _ in range(0, object_num):
+        original_pcd_transform_index_arr.append(np.random.randint(rot_arr.shape[0], size=test_group))
+        transformed_pcd_transform_index_arr.append(np.random.randint(rot_arr.shape[0], size=test_group))
+   
     
-    transform_index_pcd_transformed_1 = np.random.randint(rot_arr.shape[0], size=test_group)
-    transform_index_pcd_transformed_2 = np.random.randint(rot_arr.shape[0], size=test_group)
+    for index_test in range(0,test_group):
+        pcd_original_xyz = []
+        pcd_transformed_xyz = []
+        for index_object in range(0, object_num):
+            original_pcd_transform_index = original_pcd_transform_index_arr[index_object]
+            transformed_pcd_transform_index  = transformed_pcd_transform_index_arr[index_object]
 
-    for index in range(0,test_group):
-        # 1*6 : "Trans_x","Trans_y","Trans_z","Rot_x(deg)","Rot_y(deg)","Rot_z(deg)"
-        t_and_r_pcd_1 = rot_arr[transform_index_pcd_1[index]] 
-        pcd1, _ = transformPcd(pcd1_arr[index], t_and_r_pcd_1)
-        pcd1_xyz = np.asarray(pcd1.points)
 
-        t_and_r_pcd_2 = rot_arr[transform_index_pcd_2[index]] 
-        pcd2, _ = transformPcd(pcd2_arr[index], t_and_r_pcd_2)
-        pcd2_xyz = np.asarray(pcd2.points)
+            # 1*6 : "Trans_x","Trans_y","Trans_z","Rot_x(deg)","Rot_y(deg)","Rot_z(deg)"
+            t_and_r_pcd_original = rot_arr[original_pcd_transform_index[index_test]] 
+            pcd_original, _ = transformPcd(pcd_arrs[index_object][index_test], t_and_r_pcd_original)
+            pcd_original_xyz.append(np.asarray(pcd_original.points))
 
-        t_and_r_pcd_transformed_1 = rot_arr[transform_index_pcd_transformed_1[index]] 
-        transformed_pcd1, transform_matrix_1 = transformPcd(pcd1, t_and_r_pcd_transformed_1)
-        transformed_pcd1_xyz = np.asarray(transformed_pcd1.points)
+            t_and_r_pcd_transformed = rot_arr[transformed_pcd_transform_index[index_test]] 
+            pcd_transformed, _ = transformPcd(pcd_original, t_and_r_pcd_transformed)
+            pcd_transformed_xyz.append(np.asarray(pcd_transformed.points))
 
-        t_and_r_pcd_transformed_2 = rot_arr[transform_index_pcd_transformed_2[index]] 
-        transformed_pcd2, transform_matrix_2 = transformPcd(pcd2, t_and_r_pcd_transformed_2)
-        transformed_pcd2_xyz = np.asarray(transformed_pcd2.points)
-
-        np.savez("./test_cases/test_case_"+str(index)+".npz", pcd1_xyz=pcd1_xyz,pcd2_xyz=pcd2_xyz,
-            transformed_pcd1_xyz=transformed_pcd1_xyz,transformed_pcd2_xyz=transformed_pcd2_xyz,
-            transform_matrix_1=transform_matrix_1, 
-            transform_matrix_2=transform_matrix_2)
+        np.savez("./test_cases/test_case_"+str(index_test)+".npz", pcd_original=pcd_original_xyz, 
+                pcd_transformed=pcd_transformed_xyz)
 
 '''
-plotPcdSideBySide : plot four pcd, the original 2 in one plot and the transformed 2 in one plot
+plotPcdSideBySide : plot four pcd, the original pcd in one plot and the transformed pcd in one plot
 Input : 
-    pcd_original -> [original pcd1 , original pcd 2]
-    pcd_transformed -> [transformed pcd1 , transformed pcd 2]
+    test_case -> the test case which one wants to visualize
 Output:
-    Showing a plot
+    modified test_cases written to the "./test_cases/" folder
 '''
-def plotPcdSideBySide(pcd_original, pcd_transformed):
+def plotPcdSideBySide(testcase):
     fig = plt.figure(figsize=plt.figaspect(0.5))
     ax = fig.add_subplot(1, 2, 1, projection='3d')
 
-    ax.scatter(pcd_original[0][:,0], pcd_original[0][:,1], pcd_original[0][:,2], marker='o', c='r')
-    ax.scatter(pcd_original[1][:,0], pcd_original[1][:,1], pcd_original[1][:,2], marker='o', c='b')
- 
-    ax = fig.add_subplot(1, 2, 2, projection='3d')
-    ax.scatter(pcd_transformed[0][:,0], pcd_transformed[0][:,1], pcd_transformed[0][:,2], marker='o', c='r')
-    ax.scatter(pcd_transformed[1][:,0], pcd_transformed[1][:,1], pcd_transformed[1][:,2], marker='o', c='b')
- 
+    pcd_original = testcase["pcd_original"]
+    pcd_transformed = testcase["pcd_transformed"]
 
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-    plt.show()
-
-'''
-plotPcdSideBySide : plot two pcd in one picture
-Input : 
-    pcd_1 -> one of the pcd tio plot
-    pcd_2 -> the other pcd to plot
-Output:
-    Showing a plot
-'''
-def plotPcd(pcd_1, pcd_2):
-    fig = plt.figure()
-    plt3d = fig.gca(projection='3d')
-    ax = plt.gca()
-
-    ax.scatter(pcd_2[:,0], pcd_2[:,1], pcd_2[:,2], marker='o', c='r')
-    ax.scatter(pcd_1[:,0], pcd_1[:,1], pcd_1[:,2], marker='o', c='b')
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-    plt.show()
-
-'''
-checkCorrectness : check the correctness of calculated transform,
-Input : 
-    test_case -> the test case which one wants to check
-    transform_1 -> the first object's calculated transform
-    transform_2 -> the second object's calculated transform
-Output:
-    Showing a plot
-'''
-def checkCorrectness(test_case, transform_1, transform_2):
-    pcd1_homo = np.hstack((test_case["pcd1_xyz"],np.ones((test_case["pcd1_xyz"].shape[0],1))))
-    transformed_pcd1_homo = np.matmul(transform_1,pcd1_homo.T)
-    transformed_pcd1 = transformed_pcd1_homo[:3,:]/(transformed_pcd1_homo[-1,:].reshape((1,transformed_pcd1_homo.shape[1])))
-    plotPcd(transformed_pcd1, test_case["transformed_pcd1_xyz"])
-
-    pcd2_homo = np.hstack((test_case["pcd2_xyz"],np.ones((test_case["pcd2_xyz"].shape[0],1))))
-    transformed_pcd2_homo = np.matmul(transform_2,pcd2_homo.T)
-    transformed_pcd2 = transformed_pcd2_homo[:3,:]/(transformed_pcd2_homo[-1,:].reshape((1,transformed_pcd2_homo.shape[1])))
-    plotPcd(transformed_pcd2, test_case["transformed_pcd2_xyz"])
+    colors = cm.rainbow(np.linspace(0, 1, max(len(pcd_original),len(pcd_transformed))))
+    for index in range(0,len(pcd_original)):
+        ax.scatter(pcd_original[index][:,0], pcd_original[index][:,1], pcd_original[index][:,2], marker='o', c=colors[index])
     
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
 
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    for index in range(0,len(pcd_transformed)):
+        ax.scatter(pcd_transformed[index][:,0], pcd_transformed[index][:,1], pcd_transformed[index][:,2], marker='o', c=colors[index])
+ 
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
+'''
+add_gaussian_noise : add gaussian noise to all the pcd with noise level
+Input : 
+    test_case -> the test case which one wants to visualize
+    noise_level -> the noise level of the gaussian noise
+    test_number -> the test case's number
+Output:
+    Showing a plot
+'''
+def add_gaussian_noise(test_case ,noise_level,test_number):
+    pcd_original = test_case["pcd_original"]
+    pcd_transformed = test_case["pcd_transformed"]
+
+    for index in range(0,len(pcd_original)):
+        for row in range(0,pcd_original[index].shape[0]):
+            pcd_original[index][row][0] += np.random.normal(0,noise_level)
+            pcd_original[index][row][1] += np.random.normal(0,noise_level)
+            pcd_original[index][row][2] += np.random.normal(0,noise_level)
+        for row in range(0,pcd_transformed[index].shape[0]):
+            pcd_transformed[index][row][0] += np.random.normal(0,noise_level)
+            pcd_transformed[index][row][1] += np.random.normal(0,noise_level)
+            pcd_transformed[index][row][2] += np.random.normal(0,noise_level)
+    
+    np.savez("./test_cases/test_case_"+str(test_number)+".npz", pcd_original=pcd_original, pcd_transformed=pcd_transformed)
+
+def split_pcd(test_group, split_number, rot_file_name):
+    pcd_arr = generatePcd(test_group, 1)
+    for index in range(0,test_group):
+        pcd_split_arr = []
+        pcd = pcd_arr[index][0]
+        pcd_points = np.asarray(pcd.points)
+        pcd_points_copy= copy.deepcopy(pcd_points)
+
+        x_min = np.min(pcd_points_copy[:,0])
+        x_max = np.max(pcd_points_copy[:,0])
+        for index_split in range(1,split_number+1):
+            x_upper_limit = x_min + (x_max - x_min)/split_number*index_split
+            x_lower_limit = x_min + (x_max - x_min)/split_number*(index_split-1)
+            pcd_split_arr.append(pcd_points_copy[np.where((pcd_points_copy[:,0] < x_upper_limit) * (pcd_points_copy[:,0] >= x_lower_limit))])
+        pcd_original = [pcd_points]
+
+        rot_arr = np.loadtxt(open(rot_file_name, "rb"), delimiter=",")
+        split_tranform_index = np.random.randint(rot_arr.shape[0], size=len(pcd_split_arr))
+        pcd_split = []
+        
+        for pcd_split_index in range(0,len(pcd_split_arr)):
+            t_and_r = rot_arr[split_tranform_index[pcd_split_index]] 
+            pcd = o3d.geometry.PointCloud()
+            pcd.points = o3d.utility.Vector3dVector(pcd_split_arr[pcd_split_index])
+            pcd_transformed, _ = transformPcd(pcd, t_and_r)
+            pcd_split.append(np.asarray(pcd_transformed.points))
+
+        np.savez("./test_cases/split_pcd_"+str(index)+".npz", pcd_original=pcd_original, 
+                    pcd_transformed=pcd_split) 
+# '''
+# checkCorrectness : check the correctness of calculated transform,
+# Input : 
+#     test_case -> the test case which one wants to check
+#     transform_1 -> the first object's calculated transform
+#     transform_2 -> the second object's calculated transform
+# Output:
+#     Showing a plot
+# '''
+# def checkCorrectness(test_case, transform_1, transform_2):
+#     pcd1_homo = np.hstack((test_case["pcd1_xyz"],np.ones((test_case["pcd1_xyz"].shape[0],1))))
+#     transformed_pcd1_homo = np.matmul(transform_1,pcd1_homo.T)
+#     transformed_pcd1 = transformed_pcd1_homo[:3,:]/(transformed_pcd1_homo[-1,:].reshape((1,transformed_pcd1_homo.shape[1])))
+#     plotPcd(transformed_pcd1, test_case["transformed_pcd1_xyz"])
+
+#     pcd2_homo = np.hstack((test_case["pcd2_xyz"],np.ones((test_case["pcd2_xyz"].shape[0],1))))
+#     transformed_pcd2_homo = np.matmul(transform_2,pcd2_homo.T)
+#     transformed_pcd2 = transformed_pcd2_homo[:3,:]/(transformed_pcd2_homo[-1,:].reshape((1,transformed_pcd2_homo.shape[1])))
+#     plotPcd(transformed_pcd2, test_case["transformed_pcd2_xyz"])
+    
 if __name__ == "__main__": 
     test_group = 2
-    pcd1_arr, pcd2_arr = generatePcd(test_group)
+    object_number = 5
+    pcd_arrs = generatePcd(test_group,object_number)
 
-    generateTestCase(test_group,"./transform_matrix/rot_45.csv",pcd1_arr,pcd2_arr)
-    
-    test_case_0 = np.load("./test_cases/test_case_0.npz")
-    
+    generateTestCase(object_number, test_group,"./transform_matrix/rot_45.csv",pcd_arrs)
+
+    # Add noise
+    # test_case_0 = np.load("./test_cases/test_case_0.npz")
+    # test_case_1 = np.load("./test_cases/test_case_1.npz")
+    # add_gaussian_noise(test_case_0 ,0.03,0)
+    # add_gaussian_noise(test_case_1 ,0.03,1)
+     
+    # add_gaussian_noise(test_case_0 ,0.05,0)
+    # add_gaussian_noise(test_case_1 ,0.05,1)
+
     # Some visualization
-    o3d.visualization.draw_geometries([pcd1_arr[0], pcd2_arr[0]])
-    plotPcdSideBySide([test_case_0["pcd1_xyz"],test_case_0["pcd2_xyz"]], 
-        [test_case_0["transformed_pcd1_xyz"],test_case_0["transformed_pcd2_xyz"]])
-    plotPcd(test_case_0["pcd1_xyz"],test_case_0["pcd2_xyz"])
-    plotPcd(test_case_0["transformed_pcd1_xyz"],test_case_0["transformed_pcd2_xyz"])
+    # test_case_0 = np.load("./test_cases/test_case_0.npz")
+    # test_case_1 = np.load("./test_cases/test_case_1.npz")
+    # plotPcdSideBySide(test_case_0)
+    # plotPcdSideBySide(test_case_1)
 
-    checkCorrectness(test_case_0, test_case_0["transform_matrix_1"], test_case_0["transform_matrix_2"])
+    # Split application
+    # test_group = 1
+    # split_number = 5
+    # split_pcd(test_group, split_number, "./transform_matrix/rot_45.csv")
+    # split_pcd_0 = np.load("./test_cases/split_pcd_0.npz",allow_pickle=True)
+    # plotPcdSideBySide(split_pcd_0)
 
